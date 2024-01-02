b6ea0a27e944d0c79305116760fcdd16e93e41c9
iferret <wanggang3@staff.sina.com.cn>
2024-01-02 12:21:35 +0800

    update => no message

--- Package.swift
+++ Package.swift
@@ -1,19 +1,19 @@
-// swift-tools-version:5.3
-//
-//  Package.swift
-//
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "SKPhotoBrowser",
-    platforms: [ .iOS(.v12) ],
-    products: [ .library(name: "SKPhotoBrowser", targets: ["SKPhotoBrowser"]) ],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(name: "SKPhotoBrowser", targets: ["SKPhotoBrowser"]),
+    ],
     targets: [
-        .target(name: "SKPhotoBrowser", dependencies: ["SKPhotoBrowserObjC"], path: "SKPhotoBrowser", exclude: ["Info.plist", "Extends/ObjC"], resources: [
-            .copy("SKPhotoBrowser.bundle")
-        ]),
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(name: "SKPhotoBrowser", dependencies: ["SKPhotoBrowserObjC"], path: "SKPhotoBrowser", exclude: ["Info.plist", "Extends/ObjC"], resources: [.copy("SKPhotoBrowser.bundle")]),
         .target(name: "SKPhotoBrowserObjC", path: "SKPhotoBrowser/Extends/ObjC", publicHeadersPath: "."),
-        .testTarget(name: "SKPhotoBrowserTests", dependencies: ["SKPhotoBrowser"], path: "SKPhotoBrowserTests", exclude: ["Info.plist"])
+        .testTarget(name: "SKPhotoBrowserTests", dependencies: ["SKPhotoBrowser"]),
     ]
 )
